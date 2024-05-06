d52809eddd12358e6a8ee4366dd5152aab218906
Koh Virgil-Shaun <virgil-shaun.koh@etu.iut-tlse3.fr>
2024-05-06 14:07:57 +0200

    first edition

--- construireGraphe.py
+++ construireGraphe.py
@@ -1,6 +1,5 @@
 import networkx as nx
 import numpy as np
-# import matplotlib.pyplot as plt
 
 def estSymetrique(matrice):
     # verifier si une matrice est carree
@@ -19,20 +18,51 @@ def construireGraphe(matrice):
     else:
         G = nx.DiGraph()
         
-    for sommets in range(1, len(matrice) + 1):
-        G.add_node(str(sommets))
+    for sommets in range(len(matrice)):
+        G.add_node(sommets)
         
     for i in range(len(matrice)):
         for j in range(len(matrice[i])):
             if matrice[i][j] == 1:
-                G.add_edge(str(i), str(j))
+                G.add_edge(i, j)
                 
-    nx.draw_networkx(G)
+    nx.draw_networkx(G, node_color='blue', node_size=500, edge_color='red', 
+                     with_labels=True, font_size=10, font_color='white')
     
-M = np.array([[1, 0, 1, 1], 
-              [0, 1, 0, 0], 
-              [1, 1, 1, 1], 
-              [0, 0, 0, 1]])
+    
+    
+    
+def cheminExiste(matrice, chemin):
+    # verifier si ce chemin existe dans la matrice
+    for i in range(len(chemin) - 1):
+        if matrice[chemin[i]][chemin[i + 1]] != 1:
+            return False
+    return True
+    
+def construireChemin(matrice, chemin):
+    if estSymetrique(matrice):
+        G = nx.Graph()
+    else:
+        G = nx.DiGraph()
+        
+    for sommets in range(len(matrice)):
+        G.add_node(sommets)
+        
+    for i in range(len(matrice)):
+        for j in range(len(matrice[i])):
+            if matrice[i][j] == 1:
+                G.add_edge(i, j)
+                
+    nx.draw_networkx(G, node_color='blue', node_size=500, edge_color='red', 
+                     with_labels=True, font_size=10, font_color='white')
+    
+    
 
-construireGraphe(M)
+M = np.array([[1, 0, 1, 1], 
+              [1, 1, 0, 1], 
+              [0, 1, 0, 1], 
+              [0, 0, 1, 0]])
+
+# construireGraphe(M)
+construireChemin(M, np.array([0, 2, 1, 3]))
             
