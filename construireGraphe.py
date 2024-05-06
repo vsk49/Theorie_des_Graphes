2b835976e0afb626a935eb4c092959826e754f01
Koh Virgil-Shaun <virgil-shaun.koh@etu.iut-tlse3.fr>
2024-05-06 11:55:59 +0200

    finished construction

--- construireGraphe.py
+++ construireGraphe.py
@@ -1,12 +1,38 @@
 import networkx as nx
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-def construireGraphe(M):
-    G = nx.DiGraph()
-    for sommets in range(1, len(M) + 1):
+def estSymetrique(matrice):
+    # verifier si une matrice est carree
+    if len(matrice) != len(matrice[0]):
+        return False
+    # verifier la symmetrie d'une matrice
+    for i in range(len(matrice)):
+        for j in range(len(matrice[i])):
+            if matrice[i][j] != matrice[j][i]:
+                return False
+    return True
+
+def construireGraphe(matrice):
+    if estSymetrique(matrice):
+        G = nx.Graph()
+    else:
+        G = nx.DiGraph()
+        
+    for sommets in range(1, len(matrice) + 1):
         G.add_node(str(sommets))
         
-    for i in M:
-        for j in M[i]:
+    for i in range(len(matrice)):
+        for j in range(len(matrice[i])):
+            if matrice[i][j] == 1:
+                G.add_edge(str(i), str(j))
+                
+    nx.draw_networkx(G)
+    
+M = np.array([[1, 0, 1, 1], 
+              [0, 1, 0, 0], 
+              [1, 1, 1, 1], 
+              [0, 0, 0, 1]])
+
+construireGraphe(M)
             
