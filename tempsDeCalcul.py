12b4b277b828bc236536013b471a99859c139958
Virgil Shaun Koh <virgilskoh@gmail.com>
2024-05-13 09:39:45 +0200

    implemented TempsBF(n) and TempsDij(n)

--- tempsDeCalcul.py
+++ tempsDeCalcul.py
@@ -12,5 +12,17 @@ def TempsDij(n):
     tempsExecution = fin - debut
     return tempsExecution
 
+# retourne le temps de calcul de l'algorithme de Bellman-Ford
+def TempsBF(n):
+    M = gm.graphe(n, 1, 50)
+    debut = time.time()
+    for i in range(n):
+        acpc.BellmanFord(M, i)
+    fin = time.time()
+    tempsExecution = fin - debut
+    return tempsExecution
+    
+
 # Test de la fonction TempsDij
 print(TempsDij(100))
+print(TempsBF(100))
