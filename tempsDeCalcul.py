848b70c505b0dafc3569b6b35c2134278a81ed9b
Virgil-Shaun Koh <virgil-shaun.koh@etu.iut-tlse3.fr>
2024-05-27 11:24:14 +0200

    added log graphs

--- tempsDeCalcul.py
+++ tempsDeCalcul.py
@@ -1,49 +1,58 @@
 import genererMatrice as gm
 import algosCheminPlusCourt as acpc
 import matplotlib.pyplot as plt
+import numpy as np
 import time
 
 # retourne le temps de calcul de l'algorithme de Dijkstra
 def TempsDij(n):
     M = gm.graphe(n, 1, 200)
-    debut = time.perf_counter()
-    for _ in range(n):  # run the algorithm 100 times
-        acpc.Dijkstra(M, 0)
-    fin = time.perf_counter()
-    return (fin - debut) / n  # return the average time
+    debut = time.time()
+    for i in range(n):
+        acpc.Dijkstra(M, i)
+    fin = time.time()
+    return fin - debut
 
 # retourne le temps de calcul de l'algorithme de Bellman-Ford
 def TempsBF(n, ordre='largeur'):
     M = gm.graphe(n, 1, 200)
-    debut = time.perf_counter()
-    for _ in range(n):
-        acpc.BellmanFord(M, 0, ordre)
-    fin = time.perf_counter()
-    return (fin - debut) / n
+    debut = time.time()
+    for i in range(n):
+        acpc.BellmanFord(M, i, ordre)
+    fin = time.time()
+    return fin - debut
     
 # Test de la fonction TempsDij
-print(TempsDij(100))
-# Test de la fonction TempsBF en 3 variants
-print(TempsBF(100, 'largeur'))
-# print(TempsBF(100, 'profondeur'))
-# print(TempsBF(100, 'hasard'))
+print(TempsDij(50))
+
+# Test de la fonction TempsBF
+print(TempsBF(50, 'largeur'))
 
 # Tracer les courbes de temps de calcul
 def tracerCourbes():
-    n = [i for i in range(2, 201, 10)]
-    tempsDij = [TempsDij(i) for i in n]
-    tempsBF_largeur = [TempsBF(i, 'largeur') for i in n]
-    # tempsBF_profondeur = [TempsBF(i, 'profondeur') for i in n]
-    # tempsBF_hasard = [TempsBF(i, 'hasard') for i in n]
+    n = np.array([i for i in range(2, 201, 10)])
+    tempsDij = np.array([TempsDij(i) for i in n])
+    tempsBF_largeur = np.array([TempsBF(i, 'largeur') for i in n])
 
     plt.plot(n, tempsDij, label='Dijkstra')
     plt.plot(n, tempsBF_largeur, label='Bellman-Ford largeur')
-    # plt.plot(n, tempsBF_profondeur, label='Bellman-Ford profondeur')
-    # plt.plot(n, tempsBF_hasard, label='Bellman-Ford hasard')
     plt.xlabel('Nombre de sommets')
     plt.ylabel('Temps de calcul(s)')
     plt.title('Temps de calcul des algorithmes de plus court chemin')
     plt.legend()
     plt.show()
+
+def tracerCourbesLogLog():
+    n = np.array([i for i in range(2, 201, 10)])
+    tempsDij = np.array([TempsDij(i) for i in n])
+    tempsBF_largeur = np.array([TempsBF(i, 'largeur') for i in n])
+
+    plt.plot(np.log(n), np.log(tempsDij), label='Dijkstra')
+    plt.plot(np.log(n), np.log(tempsBF_largeur), label='Bellman-Ford largeur')
+    plt.xlabel('log(Nombre de sommets)')
+    plt.ylabel('log(Temps de calcul(s))')
+    plt.title('Temps de calcul des algorithmes de plus court chemin')
+    plt.legend()
+    plt.show()
     
-tracerCourbes()
+tracerCourbesLogLog()
