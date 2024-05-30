f75afb7227dce2214e2b7fd2481352b5d969ab0c
Virgil-Shaun Koh <virgil-shaun.koh@etu.iut-tlse3.fr>
2024-05-30 18:25:03 +0200

    calculation done

--- etudeDeSeuil.py
+++ etudeDeSeuil.py
@@ -1,6 +1,7 @@
 from forteConnexite import *
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
 def grapheSeuil():
     n = np.array([i for i in range(10, 41)])
@@ -11,4 +12,19 @@ def grapheSeuil():
     plt.legend()
     plt.show()
     
-grapheSeuil()
+def TempsSeuil(n):
+    debut = time.perf_counter()
+    seuil(n)
+    fin = time.perf_counter()
+    return fin - debut
+    
+def grapheSeuilLogLog():
+    n = np.array([i for i in range(1, 21)])
+    plt.loglog(n, [TempsSeuil(i) for i in n], label='Seuil de forte connexité')
+    plt.xlabel("taille du graphe")
+    plt.ylabel('Seuil de forte connexité')
+    plt.title('Seuil de forte connexité en fonction de la taille du graphe')
+    plt.legend()
+    plt.show()
+
+grapheSeuilLogLog()
